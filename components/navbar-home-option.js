6776f6dea652469e0a08ca3b0c7ae50011863779
Jack Denton <jackdenton21@gmail.com>
2023-06-17 17:23:30 -0500

    Nav bar

--- components/navbar-home-option.js
+++ components/navbar-home-option.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow mt-4 sm:mt-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+      <div className="flex sm:justify-center md:justify-between lg:justify-between xl:justify-between 2xl:justify-between h-16">  
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
               <Image
@@ -18,31 +18,31 @@ export default function Navbar() {
               {/* Navigation links */}
               <a
                 href="/"
-                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-5 px-1 border-b-2 font-medium text-sm font-bold"
               >
                 Home
               </a>
               <a
                 href="/about"
-                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-5 px-1 border-b-2 font-medium text-sm font-bold"
               >
                 About
               </a>
               <a
                 href="/contact"
-                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-5 px-1 border-b-2 font-medium text-sm font-bold"
               >
                 Contact
               </a>
               <a
                 href="/projects"
-                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-5 px-1 border-b-2 font-medium text-sm font-bold"
               >
                 Projects
               </a>
               <a
                 href="/experience"
-                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-5 px-1 border-b-2 font-medium text-sm font-bold"
               >
                 Experience
               </a>
