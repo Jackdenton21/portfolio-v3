aab12fde1679a104f8930742410413b2f486f7c5
Jack Denton <jackdenton21@gmail.com>
2023-06-17 16:01:00 -0500

    Nav bar

--- components/navbar.js
+++ components/navbar.js
@@ -14,34 +14,32 @@ export default function Navbar() {
                 height={40}
               />
             </div>
-            <div className="sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+            <div className="sm:-my-px sm:ml-6 sm:flex sm:space-x-8 sm:justify-center">
               {/* Navigation links */}
-              <div className="flex sm:justify-center">
-                <a
-                  href="/about"
-                  className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
-                >
-                  About
-                </a>
-                <a
-                  href="/contact"
-                  className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
-                >
-                  Contact
-                </a>
-                <a
-                  href="/projects"
-                  className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
-                >
-                  Projects
-                </a>
-                <a
-                  href="/experience"
-                  className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
-                >
-                  Experience
-                </a>
-              </div>
+              <a
+                href="/about"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+              >
+                About
+              </a>
+              <a
+                href="/contact"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+              >
+                Contact
+              </a>
+              <a
+                href="/projects"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+              >
+                Projects
+              </a>
+              <a
+                href="/experience"
+                className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm font-bold"
+              >
+                Experience
+              </a>
             </div>
           </div>
         </div>
