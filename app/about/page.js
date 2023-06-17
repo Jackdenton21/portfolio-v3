a4b2b9182377f0adb7e5212c4e14606cc2c40e2b
Jack Denton <jackdenton21@gmail.com>
2023-06-17 15:33:34 -0500

    Experience page

--- app/about/page.js
+++ app/about/page.js
@@ -3,61 +3,64 @@ import Navbar from '/components/navbar-home-option';
 
 export default function Home() {
   return (
-    
     <div className="bg-gray-100 min-h-screen">
-    <Navbar/>
-    <div className="py-12 px-4 sm:px-6 lg:px-8">
+      <Navbar />
+      <div className="py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-4xl mx-auto">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">About Me</h1>
 
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">About Me</h1>
+          <div className="bg-white shadow-md rounded-md p-6">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">Education</h2>
+            <p className="text-gray-700 mb-4">
+              I study Computer Science at McGill University. Some relevant courses I have taken include Algorithms and
+              Data Structures, Software Design Patterns, Computer Systems, Functional Programming, Concurrent Systems,
+              Information Management, and High Tech Venture Creation.
+            </p>
+          </div>
 
-        <div className="bg-white shadow-md rounded-md p-6">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Education</h2>
-          <p className="text-gray-700 mb-4">I study Computer Science at McGill University. Some relevant courses I have taken include Algorithms and Data Structures, Software Design Patterns, Computer Systems, Functional Programming, Concurrent Systems, Information Management, and High Tech Venture Creation.</p>
-          
-
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Technical Skills</h2>
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Languages</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">Python</li>
-                <li className="text-gray-700">Java</li>
-                <li className="text-gray-700">C</li>
-                <li className="text-gray-700">OCaml</li>
-                <li className="text-gray-700">SQL (MySQL)</li>
-                <li className="text-gray-700">HTML/CSS</li>
-                <li className="text-gray-700">JavaScript</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Frameworks</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">Expo</li>
-                <li className="text-gray-700">Next.js</li>
-                <li className="text-gray-700">Flask</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Developer Tools</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">GitHub</li>
-                <li className="text-gray-700">Google Firebase (Firestore)</li>
-                <li className="text-gray-700">VS Code</li>
-                <li className="text-gray-700">IntelliJ</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Libraries</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">NumPy</li>
-                <li className="text-gray-700">Matplotlib</li>
-              </ul>
+          <div className="bg-white shadow-md rounded-md p-6 mt-6">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">Technical Skills</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">Languages</h3>
+                <ul className="list-disc list-inside">
+                  <li className="text-gray-700">Python</li>
+                  <li className="text-gray-700">Java</li>
+                  <li className="text-gray-700">C</li>
+                  <li className="text-gray-700">OCaml</li>
+                  <li className="text-gray-700">SQL (MySQL)</li>
+                  <li className="text-gray-700">HTML/CSS</li>
+                  <li className="text-gray-700">JavaScript</li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">Frameworks</h3>
+                <ul className="list-disc list-inside">
+                  <li className="text-gray-700">Expo</li>
+                  <li className="text-gray-700">Next.js</li>
+                  <li className="text-gray-700">Flask</li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">Developer Tools</h3>
+                <ul className="list-disc list-inside">
+                  <li className="text-gray-700">GitHub</li>
+                  <li className="text-gray-700">Google Firebase (Firestore)</li>
+                  <li className="text-gray-700">VS Code</li>
+                  <li className="text-gray-700">IntelliJ</li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">Libraries</h3>
+                <ul className="list-disc list-inside">
+                  <li className="text-gray-700">NumPy</li>
+                  <li className="text-gray-700">Matplotlib</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- app/experience/page.js
+++ app/experience/page.js
@@ -1,63 +1,176 @@
+// pages/work-experience.js
+"use client";
+
 import React from 'react';
-import Navbar from '/components/navbar-home-option';
+import { motion } from 'framer-motion';
+import Navbar from '/components/navbar';
 
-export default function Home() {
+const WorkExperiencePage = () => {
+  const fadeInVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
-    
-    <div className="bg-gray-100 min-h-screen">
-    <Navbar/>
-    <div className="py-12 px-4 sm:px-6 lg:px-8">
-
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">About Me</h1>
-
-        <div className="bg-white shadow-md rounded-md p-6">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Education</h2>
-          <p className="text-gray-700 mb-4">I study Computer Science at McGill University. Some relevant courses I have taken include Algorithms and Data Structures, Software Design Patterns, Computer Systems, Functional Programming, Concurrent Systems, Information Management, and High Tech Venture Creation.</p>
-          
-
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Technical Skills</h2>
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Languages</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">Python</li>
-                <li className="text-gray-700">Java</li>
-                <li className="text-gray-700">C</li>
-                <li className="text-gray-700">OCaml</li>
-                <li className="text-gray-700">SQL (MySQL)</li>
-                <li className="text-gray-700">HTML/CSS</li>
-                <li className="text-gray-700">JavaScript</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Frameworks</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">Expo</li>
-                <li className="text-gray-700">Next.js</li>
-                <li className="text-gray-700">Flask</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Developer Tools</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">GitHub</li>
-                <li className="text-gray-700">Google Firebase (Firestore)</li>
-                <li className="text-gray-700">VS Code</li>
-                <li className="text-gray-700">IntelliJ</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">Libraries</h3>
-              <ul className="list-disc list-inside">
-                <li className="text-gray-700">NumPy</li>
-                <li className="text-gray-700">Matplotlib</li>
-              </ul>
-            </div>
-          </div>
+    <div className="min-h-screen bg-gray-100">
+        <Navbar></Navbar>
+      <motion.header
+        className="bg-gray-200 py-8"
+        initial="hidden"
+        animate="visible"
+        variants={fadeInVariants}
+        transition={{ duration: 0.6 }}
+      >
+        <div className="container mx-auto px-4 text-center">
+          <motion.h1
+            className="text-3xl font-bold"
+            layoutId="pageTitle"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+          >
+            Work Experience
+          </motion.h1>
         </div>
-      </div>
-    </div>
+      </motion.header>
+      <motion.main
+        className="container mx-auto px-4 py-8"
+        initial="hidden"
+        animate="visible"
+        variants={fadeInVariants}
+        transition={{ duration: 0.6, delay: 0.4 }}
+      >
+ <section className="bg-white rounded-lg shadow-lg p-6 mb-8 flex">
+  <div className="w-1/4">
+    <img
+      src="/images/twalmart.png" // Replace with the actual image URL or source
+      alt="Company A Logo"
+      className="w-full h-auto object-cover"
+    />
+    <motion.h2
+      className="text-xl font-bold mb-2 text-center"
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, delay: 0.4 }}
+    >
+      Walmart Global Tech
+    </motion.h2>
+    <p className="text-center">Software Engineer Intern</p>
+    <p className="text-center">May 2023 - Present</p>
+  </div>
+  <div className="flex-1 ml-4">
+  <ul style={{ listStyleType: 'disc', paddingLeft: '1.5rem', marginTop: '1rem' }}>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.6 }}
+  >
+    Currently building a module for the Supply Chain Tracker API that will store data on the unit level for all prescription pharmacy items as required by the Supply Chain Safety Act that comes into place Nov. 2023.
+  </motion.li>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.7 }}
+    style={{ marginTop: '1rem' }}  >
+
+    Working with large scale systems — 189Mn Rx units processed and shipped annually.
+  </motion.li>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.8 }}
+    style={{ marginTop: '1rem' }}
+  >
+    Technologies involved include Spring Boot, Kafka, CassandraDB (NoSQL), and Google BigQuery.
+  </motion.li>
+</ul>
+  </div>
+</section>
+<section className="bg-white rounded-lg shadow-lg p-6 mb-8 flex">
+  <div className="w-1/4 text-center">
+    <img
+      src="/images/tfg.png" // Replace with the actual image URL or source
+      alt="Company A Logo"
+      className="w-20 h-20 object-cover inline-block"
+    />
+    <motion.h2
+      className="text-xl font-bold mb-2 text-center"
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, delay: 0.4 }}
+    >
+      Tech For Good
+    </motion.h2>
+    <p className="text-center">Software Engineer Intern</p>
+    <p className="text-center">June 2022 - August 2022</p>
+  </div>
+  <div className="flex-1 ml-4">
+  <ul style={{ listStyleType: 'disc', paddingLeft: '1.5rem', marginTop: '1rem' }}>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.6 }}
+  >
+Designed and implemented a full-stack web application using Next.js, AWS, and Flask to display real-time data for Tech For Good’s deployed networks.  </motion.li>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.7 }}
+    style={{ marginTop: '1rem' }}  >
+
+Integrated the MapBox GL JS API to create a visually compelling representation of the data.  </motion.li>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.8 }}
+    style={{ marginTop: '1rem' }}
+  >
+Utilized GitHub for effective version control and project management, ensuring timely completion of tasks and seamless collaboration with team members.  </motion.li>
+</ul>
+  </div>
+</section>
+<section className="bg-white rounded-lg shadow-lg p-6 mb-8 flex">
+  <div className="w-1/4 text-center">
+    <img
+      src="/images/d5.png" // Replace with the actual image URL or source
+      alt="Company A Logo"
+      className="w-60 h-40 inline-block"
+      style={{ marginTop: '-2rem' }}
+    />
+    <motion.h2
+      className="text-xl font-bold mb-2 text-center"
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, delay: 0.4 }}
+    >
+      Droga5
+    </motion.h2>
+    <p className="text-center">Advertising Intern</p>
+    <p className="text-center">July 2019 - August 2019</p>
+  </div>
+  <div className="flex-1 ml-4">
+  <ul style={{ listStyleType: 'disc', paddingLeft: '1.5rem', marginTop: '1rem' }}>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.6 }}
+  >
+Collaborated with multinational brands on a variety of marketing campaigns and initiatives.  </motion.li>
+  <motion.li
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, delay: 0.7 }}
+    style={{ marginTop: '1rem' }}  >
+
+Shadowed producers creating new ads.  </motion.li>
+ 
+</ul>
+  </div>
+</section>
+        {/* Add more sections for additional work experiences */}
+        </motion.main>
     </div>
   );
-}
+};
+
+export default WorkExperiencePage;
