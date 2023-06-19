25ee62bb14a89f519ddf767de18fffdec06848ae
Jack Denton <jackdenton21@gmail.com>
2023-06-18 20:44:36 -0500

    Abt page

--- app/about/page.js
+++ app/about/page.js
@@ -3,44 +3,57 @@ import React from 'react';
 import Navbar from '/components/navbar';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-
 export default function Home() {
   const fadeInVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
   };
-
   const sections = [
     {
-      title: 'Languages',
-      images: [
-        '/images/tech/python.png',
-        '/images/tech/java.png',
-        '/images/tech/ocaml.png',
-        '/images/tech/c.png',
-        '/images/tech/html.png',
-        '/images/tech/js.png',
-        '/images/tech/css.png',
-      ],
-    },
-    {
-      title: 'Frameworks',
-      images: [
-        '/images/tech/springboot.png',
-        '/images/tech/nextjs.png',
-        '/images/tech/expo.png',
-        '/images/tech/pngwing.com.png',
-        '/images/tech/flask.png',
-
-      ],
-    },
-    // Add more sections as needed
-  ];
-
+        title: 'Languages',
+        images: [
+          '/images/tech/python.png',
+          '/images/tech/java.png',
+          '/images/tech/ocaml.png',
+          '/images/tech/c.png',
+          '/images/tech/html.png',
+          '/images/tech/js.png',
+          '/images/tech/css.png',
+        ],
+      },
+      {
+        title: 'Developer Tools',
+        images: [
+          '/images/tech/github.png',
+          '/images/tech/jira.png',
+          '/images/tech/kubernetes.png',
+          '/images/tech/docker.png',
+        ],
+      },
+      {
+        title: 'Database Related',
+        images: [
+          '/images/tech/kafka.png',
+          '/images/tech/mysql.png',
+          '/images/tech/cassandra.png',
+          '/images/tech/firestore.png',
+          '/images/tech/bigquery.png',
+        ],
+      },
+      {
+        title: 'Frameworks',
+        images: [
+          '/images/tech/springboot.png',
+          '/images/tech/nextjs.png',
+          '/images/tech/expo.png',
+          '/images/tech/pngwing.com.png',
+          '/images/tech/flask.png',
+        ],
+      },
+    ];
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
-
       <motion.header
         className="bg-gray-200 py-8"
         initial="hidden"
@@ -60,7 +73,6 @@ export default function Home() {
           </motion.h1>
         </div>
       </motion.header>
-
       <div>
         {sections.map((section, index) => (
           <section key={index} className="mt-8">
@@ -68,50 +80,74 @@ export default function Home() {
             <div
               className={`image-grid ${section.title === 'Frameworks' ? 'frameworks-grid' : ''}`}
             >
-              {section.images.map((image, index) => (
-                <div key={index} className="image-wrapper">
-                  <div className="image-container">
-                    <Image src={image} alt="Image" width={50} height={50} style={{objectFit:"cover"}/>
-                  </div>
-                </div>
-              ))}
+               {section.images.map((image, index) => (
+                 <div key={index} className="image-wrapper">
+                   <div className="image-container">
+                     <Image src={image} alt="Image" layout="fill" objectFit="contain" />
+                   </div>
+                 </div>
+               ))}
             </div>
           </section>
         ))}
       </div>
-
       <style jsx>{`
-        .image-grid {
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-          grid-gap: 1rem;
-          padding: 0 10rem; /* Add padding to the sides */
-        }
+  .image-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 1rem;
+    padding: 0 10rem; /* Add default padding to the sides */
+  }
 
-        .frameworks-grid {
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(150px, 1fr)); /* Adjust the minimum width of each column */
-          grid-gap: 5rem; /* Increase the gap between images */
-          padding: 0 13rem; /* Add padding to the sides */
-        }
+  .frameworks-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr)); /* Adjust the minimum width of each column */
+    grid-gap: 5rem; /* Increase the gap between images */
+    padding: 0 13rem; /* Add default padding to the sides */
+  }
 
-        .image-wrapper {
-          position: relative;
-          height: 75px; /* Set the desired height */
-        }
+  .image-wrapper {
+    position: relative;
+    height: 75px; /* Set the desired height */
+    margin-bottom: 20px; /* Add padding to the bottom of each image */
+  }
 
-        .framework-wrapper {
-          position: relative;
-          height: 50px; /* Set the desired height */
-        }
+  .framework-wrapper {
+    position: relative;
+    height: 30px; /* Set the desired height */
+    margin-bottom: 20px; /* Add padding to the bottom of each image */
+  }
 
+  /* Responsive Padding */
+  @media (max-width: 640px) {
+    .image-grid,
+    .frameworks-grid {
+      padding: 0 4rem; /* Adjust the padding for small screens */
 
+    }
+  }
 
-        .image-container {
-          width: 100%;
-          height: 100%;
-        }
-      `}</style>
-    </div>
+  @media (min-width: 641px) and (max-width: 1024px) {
+    .image-grid,
+    .frameworks-grid {
+      padding: 0 2rem; /* Adjust the padding for medium screens */
+    }
+  }
+
+  @media (min-width: 1025px) {
+    .image-grid,
+    .frameworks-grid {
+      padding: 0 10rem; /* Adjust the padding for large screens */
+    }
+  }
+
+  .image-container {
+    width: 100%;
+    height: 100%;
+  }
+`}</style>
+
+     </div>
+
   );
 }
