f79245509645b175c4f5eaf90d14c44178d403f8
Jack Denton <jackdenton21@gmail.com>
2023-06-23 00:14:11 -0500

    Added analytics

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}
-      
+      <Analytics />
+
       <footer className="text-center py-4 text-sm">
         <p>&copy; {new Date().getFullYear()} Developed using Next.JS and TailWind CSS. Deployed with Vercel. </p>
       </footer>
