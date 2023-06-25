ec3e18475e1e570b3b47ab841d8201bbb1de2472
Jack Denton <jackdenton21@gmail.com>
2023-06-25 17:02:00 -0500

    Resume

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,12 +1,21 @@
 "use client";
-;import React from 'react';
-import Navbar from '/components/navbar';
-import '../background.css';
+import React from "react";
+import Navbar from "/components/navbar";
+import "../background.css";
 
 export default function Home() {
   return (
     <div>
-    <Navbar/>
-     </div>
+      <Navbar />
+      <div className="flex justify-center">
+      <embed
+        src="/Resume.pdf"
+        type="application/pdf"
+        width="75%"
+        style={{ height: "80vh" }}
+
+      />
+      </div>
+    </div>
   );
 }
